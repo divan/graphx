e2caadb60a3d2044d6bea4d56a7203d1ebefa96a
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-10-24 19:17:40 +0200

    Fix split-brain generator

--- generation/net/split_brain.go
+++ generation/net/split_brain.go
@@ -31,10 +31,12 @@ func (d *SplitBrainGenerator) Generate() *graph.Graph {
 	gen := NewIPGenerator(d.startIP)
 
 	// generate hosts for left subnetwork
+	nodeIPs := map[int]string{}
 	for i := 0; i < d.hosts/2; i++ {
 		ip := gen.NextAddress()
 		node := NewNode(ip)
 		g.AddNode(node)
+		nodeIPs[i] = ip
 	}
 
 	// generate links
@@ -42,25 +44,36 @@ func (d *SplitBrainGenerator) Generate() *graph.Graph {
 		for j := 0; j < d.connections; j++ {
 			idx, err := nextIdx(g, i, 0, d.hosts)
 			if err == nil {
-				addLink(g, i, idx)
+				from, to := nodeIPs[i], nodeIPs[idx]
+				if !g.LinkExists(from, to) {
+					g.AddLink(from, to)
+				}
 			}
 		}
 	}
 
 	// right subnetwork
-	for i := 0; i < d.hosts/2; i++ {
+	for i := d.hosts / 2; i < d.hosts; i++ {
 		ip := gen.NextAddress()
 		node := NewNode(ip)
 		g.AddNode(node)
+		nodeIPs[i] = ip
 	}
 	for i := d.hosts / 2; i < d.hosts; i++ {
 		for j := 0; j < d.connections; j++ {
 			idx, err := nextIdx(g, i, d.hosts/2, d.hosts)
 			if err == nil {
-				addLink(g, i, idx)
+				from, to := nodeIPs[i], nodeIPs[idx]
+				if !g.LinkExists(from, to) {
+					g.AddLink(from, to)
+				}
 			}
 		}
 	}
+
+	// add one link between two subnetworks
+	from, to := nodeIPs[d.hosts/4], nodeIPs[d.hosts*3/4]
+	g.AddLink(from, to)
 
 	return g
 }
