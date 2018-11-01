27188df22e6b3708d257b5497b8a8dc2688d61fc
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-11-01 17:39:23 +0100

    Add benchmarks for layout

--- layout/layout_test.go
+++ layout/layout_test.go
@@ -1,6 +1,13 @@
 package layout
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/divan/graphx/generation/basic"
@@ -45,4 +52,98 @@ func TestLayoutAdd(t *testing.T) {
 	if len(l.positions) != 3 {
 		t.Fatalf("positions expected to be of %d length, but is of %d", 3, len(l.objects))
 	}
+}
+
+func BenchmarkUpdatePositions(b *testing.B) {
+	files, err := readTestData()
+	if err != nil {
+		b.Fatalf("Failed to read testdata: %v", err)
+	}
+
+	for _, file := range files {
+		g, err := FromD3JSON(file)
+		if err != nil {
+			b.Fatalf("Failed to build graph: %v", err)
+		}
+		l := New(g, DefaultConfig)
+		b.Run(file, func(b *testing.B) {
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				l.UpdatePositions()
+			}
+		})
+	}
+}
+
+// readTestData returns filenames (with relative path)
+// for files in testdata/ directory.
+func readTestData() ([]string, error) {
+	files, err := ioutil.ReadDir("testdata")
+	if err != nil {
+		return nil, fmt.Errorf("readdir: %s", err)
+	}
+
+	var ret []string
+	for _, file := range files {
+		path := filepath.Join("testdata", file.Name())
+		ret = append(ret, path)
+	}
+	return ret, nil
+}
+
+// TODO(divan): remove this from here, once package formats is refactored,
+// so there is no circular dependency.
+
+// FromD3JSON creates a graph from the given JSON file.
+// It recognizes simple JSON structure suitable for D3 examples,
+// basically just `id`, `group` and `weight` fields for nodes.
+func FromD3JSON(file string) (*graph.Graph, error) {
+	fd, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close() //nolint: errcheck
+
+	return FromD3JSONReader(fd)
+}
+
+// FromD3JSONReader creates a graph from the given JSON file.
+func FromD3JSONReader(r io.Reader) (*graph.Graph, error) {
+	// decode into temporary struct to process
+	var res struct {
+		Nodes []*graph.BasicNode `json:"nodes"`
+		Links []*struct {
+			Source string `json:"source"`
+			Target string `json:"target"`
+		} `json:"links"`
+	}
+	err := json.NewDecoder(r).Decode(&res)
+	if err != nil {
+		return nil, fmt.Errorf("decode JSON: %v", err)
+	}
+
+	if len(res.Nodes) == 0 {
+		return nil, errors.New("empty graph")
+	}
+
+	// convert links IDs into indices
+	g := graph.NewGraphMN(len(res.Nodes), len(res.Links))
+
+	for _, node := range res.Nodes {
+		g.AddNode(node)
+	}
+
+	g.UpdateCache()
+
+	for _, link := range res.Links {
+		err := g.AddLink(link.Source, link.Target)
+		if err != nil {
+			return nil, fmt.Errorf("add link: %v", err)
+		}
+	}
+
+	g.UpdateCache()
+
+	return g, err
 }
